74322237ef6552ce3ab2775760368932217ca02e
srhkami <160311248+srhkami@users.noreply.github.com>
2024-08-30 09:50:09 +0800

    1130830

--- data/statistics.js
+++ data/statistics.js
@@ -1,33 +1,38 @@
 import { allShift, members, info } from "./object.js";
 
-function getOvertime(){
-  members.forEach((member,no)=>{
+function getOvertime() {
+  members.forEach((member, no) => {
     let monthOvertime = 0;
-    Object.values(allShift).forEach((list)=>{
-      let dayOvertime = 0
-      list.forEach(value=>{
-        if(value.name == member){
-          dayOvertime += 1;
+    Object.values(allShift).forEach((list, day) => {
+      if (day == 0) {
+        // pass
+      } else {
+        let dayOvertime = 0
+        list.forEach(value => {
+          if (value.name == member) {
+            dayOvertime += 1;
+          }
+        })
+        if (dayOvertime > 8) {
+          monthOvertime = monthOvertime + dayOvertime - 8;
         }
-      })
-      if(dayOvertime>8){
-        monthOvertime = monthOvertime + dayOvertime - 8;
+        else if (dayOvertime < 8) {
+          monthOvertime += dayOvertime;
+        }
       }
-      else if(dayOvertime<8){
-        monthOvertime += dayOvertime;
-      }
-    $(`#member-${no}`).html(monthOvertime);
+
+      $(`#member-${no}`).html(monthOvertime);
     })
   })
 }
 
-function getNight(){
-  members.forEach((member,no)=>{
+function getNight() {
+  members.forEach((member, no) => {
     let monthNightTime = 0;
-    Object.values(allShift).forEach((list)=>{
+    Object.values(allShift).forEach((list) => {
       let dayNightTime = 0
-      list.forEach(value=>{
-        if(value.name == member && value.time < 6 && value.shift == '巡邏'){
+      list.forEach(value => {
+        if (value.name == member && value.time < 6 && value.shift == '巡邏') {
           dayNightTime += 1;
         }
       })
@@ -40,16 +45,16 @@ function getNight(){
 
 // 主程式
 $('#info').html(info);
-$('#item-select').on('change',()=>{
+$('#item-select').on('change', () => {
   let itemText = $('#item-select option:selected').text();
-  if (itemText == '請選擇統計項目'){
+  if (itemText == '請選擇統計項目') {
     alert('請選擇統計項目！')
     return
   }
-  else if(itemText == '加班時數'){
+  else if (itemText == '加班時數') {
     getOvertime();
   }
-  else{
+  else {
     getNight();
   }
 
